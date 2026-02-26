cf6f3a3b0d34d33e7878c0c5b69e7ce01a93398c
HAGENSV22 <HagenSV22@gcc.edu>
2026-02-26 12:15:36 -0500

    Improve layout

--- src/app/bingo/preview/page.tsx
+++ src/app/bingo/preview/page.tsx
@@ -5,7 +5,6 @@ import Button from "@mui/material/Button"
 import { useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
 import styles from "./page.module.css"
-import Stack from "@mui/material/Stack"
 import { Edit, PlayCircle, Print, Refresh, Share } from "@mui/icons-material"
 import { getBingoCard } from "@/utility/bingo/bingo_storage"
 import { editCard, shareCard } from "@/utility/bingo/navigation"
@@ -51,49 +50,51 @@ export default function Page(){
         </div>
 
         <h1 className={styles.bingoTitle}>{name}</h1>
-        <Box sx={{ width: "100%", overflowX: "scroll"}}>
         <BingoCard
             seed={seed}
             values={valuePool}
         ></BingoCard>
-        </Box>
 
         <div className="no-print">
         <Spacer height={"20px"} />
 
         {/* Action Buttons */}
-        <Grid container spacing={1} direction={"row"}>
-            <Grid size={{ xs: 6, md: 3 }} display="flex" justifyContent="center">
+        <Grid container spacing={2} direction={"row"}>
+            <Grid size={{ xs: 6 }} display="flex" justifyContent="center">
                 <Button 
                 variant="contained"
+                sx={{ width: "100%" }}
                 onClick={() => { 
                     window.location.href = editCard(id)
                 }}
             ><Edit />Edit</Button>
             </Grid>
 
-            <Grid size={{ xs: 6, md: 3 }} display="flex" justifyContent="center">
+            <Grid size={{ xs: 6 }} display="flex" justifyContent="center">
                 <Button 
                 variant="contained"
+                sx={{ width: "100%" }}
                 onClick={() => {
                     setSeed(Math.floor((10**10)*Math.random()))
                 }}  
             ><Refresh />Regenerate</Button>
             </Grid>
-
-            <Grid size={{ xs: 6, md: 3}} display="flex" justifyContent="center">
+{/* 
+            <Grid size={{ xs: 4 }} display="flex" justifyContent="center">
             <Button 
                 variant="contained"
                 className="no-print"
+                sx={{ width: "100%" }}
                 onClick={() => {
                     window.location.href = "/bingo/play"
                 }}
             ><PlayCircle />Play</Button>
-            </Grid>
+            </Grid> */}
 
-            <Grid size={{ xs: 6, md: 3 }} display="flex" justifyContent="center">
+            <Grid size={{ xs: 6 }} display="flex" justifyContent="center">
                 <Button 
                 variant="contained"
+                sx={{ width: "100%" }}
                 onClick={() => {
                     print();
                 }}
@@ -101,9 +102,10 @@ export default function Page(){
             <Print />Print</Button>
             </Grid>
 
-            <Grid size={{ xs: 6, md: 3 }} display="flex" justifyContent="center">
+            <Grid size={{ xs: 6 }} display="flex" justifyContent="center">
                 <Button 
                 variant="contained"
+                sx={{ width: "100%" }}
                 onClick={async () => {
                     await navigator.clipboard.writeText(window.location.host + shareCard(id,seed))
                     alert("Link Copied")
@@ -112,21 +114,13 @@ export default function Page(){
             <Share />Share</Button>
             </Grid>
         </Grid>
-        <Stack direction="row" justifyContent="space-evenly">
-
-            {/* <Button 
-                variant="contained"
-                className="no-print"
-                onClick={() => {
-                    window.location.href = "/bingo/play"
-                }}
-            ><PlayCircle />Play</Button> */}
-        </Stack>
 
         <Spacer height="40px" />
 
         <h2 className="text-center">Possible Values:</h2>
         {valuePool.map( (value,idx) => <p key={idx} className="text-center">{value}</p>)}
+
+        <Spacer height="20px" />
 
         </div>
 
