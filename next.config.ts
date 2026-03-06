dba5e69f2593ad6ba785d7c62b3b86d6c19f1116
HAGENSV22 <HagenSV22@gcc.edu>
2026-03-06 00:35:53 -0500

    fix config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/', // Replace with your GitHub repo name
-  assetPrefix: '/',
+  basePath: '', // Replace with your GitHub repo name
+  assetPrefix: '',
   distDir: 'out'
 };
 
