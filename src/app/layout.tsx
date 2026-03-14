87de2989e99e8de58056401ecc7fabc0c1fec757
HAGENSV22 <HagenSV22@gcc.edu>
2026-03-14 00:58:14 -0400

    update metadata, layout, and GA-id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,12 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "@/theme/mainTheme";
+import Footer from "@/components/Footer";
+import AppThemeProvider from "@/components/AppThemeProvider";
+import Box from "@mui/material/Box";
 
 export const metadata: Metadata = {
-  title: "HagenSV Games",
+  title: "CustomMade Games",
   description: "A collection of web-based games to play offline",
   other: {
     "google-site-verification": "AEBD389WngD937lWHS-pgY5RbAaPOHABFRBWM84bm7g"
@@ -23,19 +26,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <GoogleAnalytics gaId="G-8FE7FCWCX8" />
+        <GoogleAnalytics gaId="G-HFESTGRHQL" />
       </head>
       <body>
         <AppRouterCacheProvider>
-        <ThemeProvider theme={theme}>
+        <AppThemeProvider>
         <Navigation />
           <Suspense>
             {children}
           </Suspense>
-        {/* <Footer /> */}
-        </ThemeProvider>
+        <Box sx={{ height: "50px" }}></Box>
+        <Footer />
+        </AppThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
   );
-}
+}[]
