e3bece1cdd543ceba510352176c49c642f81dff0
HAGENSV22 <HagenSV22@gcc.edu>
2026-03-14 00:58:48 -0400

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,26 @@
 
 import { useEffect } from "react";
 import { bingoHome } from "@/utility/bingo/navigation";
+import Link from "next/link";
+import Box from "@mui/material/Box";
 
 export default function Home() {
 
-  useEffect( () => {
-    window.location.href = bingoHome()
-  })
+  // useEffect( () => {
+  //   window.location.href = bingoHome()
+  // })
+
+  return (
+    <Box sx={{ margin: { xs: "0 5%", md: "0 10% "}}}>
+      <main>
+        <h1>CustomMade Games</h1>
+          <h2>About</h2>
+          <p>This website is a passion project for custom games/game generators. Our philosophy is that everything should work entirely offline, meaning all data stored on your device. We strive to collect as little data as possible. If everything is stored in your browser, there's no need for user accounts. It also has the added benefit of minimizing costs on our end, allowing us to keep our website completely free.</p>
+          <p>Want to suggest a game or generator? <Link href={"mailto:contact@custommade.games"}>Send us an email!</Link></p>
+          <h2>Games / Generators</h2>
+          <h3><Link href={bingoHome()}>Bingo!</Link></h3>
+          <p>Create your own custom bingo cards. Create a list of possible values and generate as many random cards as you want! You can print them or share with your friends.</p>
+      </main>
+    </Box>
+  )
 }
