103158096062c597ff138d2abc0c7dc1a235313d
HAGENSV22 <HagenSV22@gcc.edu>
2026-03-14 23:59:37 -0400

    print layout

---
+++ src/components/bingo/PrintLayout.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { BingoCardData } from "@/types/Bingo";
+import Box from "@mui/material/Box";
+import BingoCard from "./BingoCard";
+import { useMemo } from "react";
+import crypto from "crypto"
+
+interface Props {
+    card: BingoCardData,
+    seed: number,
+    count: number
+}
+
+export default function BingoPrintLayout({ card, seed, count }: Props){
+    const cards = useMemo( () => Array.from({ length: count }, (_, i) => i), [count]);
+
+    return <Box className="print-only">
+        {cards.map( (val) =>
+            <Box key={val} sx={{ pageBreakInside: "avoid", pageBreakBefore: val == 0 ? "avoid" : "always" }}>
+                <p style={{ fontSize: "8pt", margin: 0 }}>Card {crypto.createHash('sha256').update(card.values.join("\n")).digest('hex')}</p>
+                <p style={{ fontSize: "8pt", margin: 0 }}>No. {seed}</p>
+                <h1 style={{ margin: 0, textAlign: "center" }}>{card.name}</h1>
+                <BingoCard 
+                    values={card.values}
+                    seed={seed+val}
+                />
+            </Box>
+        )}
+    </Box>
+}
