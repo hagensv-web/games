c5ded329fb9df4c1f859d42b965072a1f799a374
HAGENSV22 <HagenSV22@gcc.edu>
2026-03-18 08:41:49 -0400

    optimize imports

--- src/components/bingo/PrintButton.tsx
+++ src/components/bingo/PrintButton.tsx
@@ -5,7 +5,7 @@ import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import TextField from "@mui/material/TextField";
-import { Print } from "@mui/icons-material";
+import Print from "@mui/icons-material/Print";
 
 interface Props {
     onPrint: (count: number) => void
